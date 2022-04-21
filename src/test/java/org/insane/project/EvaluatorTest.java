a506bdd3c0b4ad160b69cf7c13fdfa9ef8b62266
Martin Bourbier <martin.bourbier@epita.fr>
2022-04-21 17:32:57 -0400

    Format edit to EvaluatorTest.java

--- src/test/java/org/insane/project/EvaluatorTest.java
+++ src/test/java/org/insane/project/EvaluatorTest.java
@@ -1,7 +1,6 @@
 package org.insane.project;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 import org.insane.project.src.ExpressionAnalysis.Arithmetic.Polynomial;

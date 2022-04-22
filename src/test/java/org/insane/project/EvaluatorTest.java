249f4e259564103acb1c612edb9ce1cd597d6a93
Martin Bourbier <martin.bourbier@epita.fr>
2022-04-22 13:55:55 -0400

    Added test for basic operations (Addition and Substraction)
    + Error management

--- src/test/java/org/insane/project/EvaluatorTest.java
+++ src/test/java/org/insane/project/EvaluatorTest.java
@@ -109,4 +109,152 @@ public class EvaluatorTest
 
         assertTrue(PolyEquals(p, expected));
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ErrorManagement_WrongVariable()
+    {
+        Computation.Compute("a");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ErrorManagement_GoodAndWrongVariable()
+    {
+        Computation.Compute(" x  a   ");
+    }
+
+    @Test
+    public void NumberOnly_Addition()
+    {
+        Polynomial p = Computation.Compute("1+3");
+
+        Polynomial expected = new Polynomial(new Monomial(1 + 3));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void NumberOnly_Add_WithWhitespace_TwoMembers()
+    {
+        Polynomial p = Computation.Compute(" 1 + 3 ");
+
+        Polynomial expected = new Polynomial(new Monomial(1 + 3));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void NumberOnly_Add_WithWhitespace_ALotOfMembers()
+    {
+        Polynomial p = Computation.Compute(" 1 + 3+ 1 + 3 + 5");
+
+        Polynomial expected = new Polynomial(new Monomial(1 + 3 + 1 + 3 + 5));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void NumberOnly_Minus()
+    {
+        Polynomial p = Computation.Compute("1-3");
+
+        Polynomial expected = new Polynomial(new Monomial(1 - 3));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void NumberOnly_Minus_WithWhiteSpace_TwoMembers()
+    {
+        Polynomial p = Computation.Compute(" 1 - 3 ");
+
+        Polynomial expected = new Polynomial(new Monomial(1 - 3));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void NumberOnly_Mix_AS_1()
+    {
+        Polynomial p = Computation.Compute("1-3+2+3");
+
+        Polynomial expected = new Polynomial(new Monomial(1 - 3 + 2 + 3));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void NumberOnly_Mix_AS_2()
+    {
+        Polynomial p = Computation.Compute(" 1 - 3+2");
+
+        Polynomial expected = new Polynomial(new Monomial(1 - 3 + 2));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void WithVariable_SimpleAddition()
+    {
+        Polynomial p = Computation.Compute("1+ x");
+
+        Polynomial expected = new Polynomial(new Monomial(1));
+        expected = Polynomial.Add(expected, new Polynomial(new Monomial(1, 1)));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void WithVariable_Mix_Result_Mix()
+    {
+        Polynomial p = Computation.Compute("1+ x + 4-x-x");
+
+        Polynomial expected = new Polynomial(new Monomial(5));
+        expected = Polynomial.Add(expected, new Polynomial(new Monomial(-1, 1)));
+        
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void WithVariable_Mix_Result_OnlyNumber()
+    {
+        Polynomial p = Computation.Compute("x + 1 -x");
+
+        Polynomial expected = new Polynomial(new Monomial(1));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test
+    public void WithVariable_Mix_Result_OnlyVariable()
+    {
+        Polynomial p = Computation.Compute("2 + x -2 + x");
+
+        Polynomial expected = new Polynomial(new Monomial(2, 1));
+
+        assertTrue(PolyEquals(p, expected));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ErrorManagement_DoubleNumber()
+    {
+        Computation.Compute("1 4+ x");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ErrorManagement_DoubleVariable()
+    {
+        Computation.Compute("1+ x x");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ErrorManagement_TrailingOperator_Plus()
+    {
+        Computation.Compute("1 - 3 +");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ErrorManagement_TrailingOperator_Minus()
+    {
+        Computation.Compute("1 - 3 -");
+    }
 }
